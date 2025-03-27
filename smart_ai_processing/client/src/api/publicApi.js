d1aa229d9bf94a97e580c46c9d9e981ace750385
Ahmad Fiqri Oemry <121015122+fiqrioemry@users.noreply.github.com>
2025-03-27 10:25:26 +0700

    Delete smart_ai_processing directory

--- smart_ai_processing/client/src/api/publicApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const publicApi = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-    "X-API-Key": import.meta.env.VITE_API_KEY,
-  },
-});
