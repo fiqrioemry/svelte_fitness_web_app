c9f14ec243c0ca416405ebd9a359cf597bd8bae1
Ahmad Fiqri Oemry <121015122+fiqrioemry@users.noreply.github.com>
2025-03-27 10:25:43 +0700

    Delete 02_fullstack_social_media_web_app directory

--- 02_fullstack_social_media_web_app/client/src/components/post/LikeCount.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-const LikeCount = ({ data }) => {
-  return <div>{data.likes > 0 && <span>{data.likes} likes</span>}</div>;
-};
-
-export default LikeCount;
--- 02_fullstack_social_media_web_app/social-media-server/middleware/apikey.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function verifyApiKey(req, res, next) {
-  const clientKey = req.header('X-API-Key');
-
-  if (!clientKey || clientKey !== process.env.SERVER_API_KEY) {
-    return res.status(403).json({ message: 'Forbidden: Invalid API Key' });
-  }
-
-  next();
-};
